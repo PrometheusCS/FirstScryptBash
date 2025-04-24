5547dd4d58c68c1c891a42675f991cd56c18a65e
Gregory Terryn <171694387+PrometheusCS@users.noreply.github.com>
2025-04-24 14:41:47 +0200

    Ajout de mon premier script pour mettre à jour Kali

---
+++ maj_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Mettre à jour la liste des paquets
+sudo apt-get update
+
+# Mettre à niveau les paquets
+sudo apt-get upgrade -y
+
+# Supprimer les paquets inutiles
+sudo apt-get autoremove -y
+
+# Nettoyer le cache des paquets
+sudo apt-get clean
+
+# Installer quelques applications de base (remplace 'nom_du_paquet' par les paquets que tu veux installer)
+# sudo apt-get install -y nom_du_paquet1 nom_du_paquet2 nom_du_paquet3
+
+echo "Mise à jour et installation terminées!"
